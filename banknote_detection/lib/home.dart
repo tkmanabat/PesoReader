5c8a494ed109afe0986a0fa88ebcc8edae39631d
tkmanabat <tkmanabat@gmail.com>
2022-06-20 22:28:08 +0800

    change background color

--- banknote_detection/lib/home.dart
+++ banknote_detection/lib/home.dart
@@ -175,7 +175,7 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
         // Wait until the controller is initialized before displaying the
         // camera preview. Use a FutureBuilder to display a loading spinner
         // until the controller has finished initializing.
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         body: Stack(children: <Widget>[
           FutureBuilder<void>(
             future: _initializeControllerFuture,
